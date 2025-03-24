ee3c46591fded099f441d761a8a187fe49875cd2
B00177535 Nora Klarin <B00177535@myTUDublin.ie>
2025-03-24 15:04:52 +0000

    member deleted

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     $router->get('members/{id}', ['uses' => 'MemberController@showOneMember']);
     $router->get('member/{id}/bookings', ['uses' => 'App\Http\Controllers\MemberController@showMemberBookings']);
     $router->post('members', ['uses' => 'MemberController@create']);
-});
-$router->put('members/{id}', ['uses' => 'MemberController@update']);
+    $router->put('members/{id}', ['uses' => 'MemberController@update']);
 $router->delete('members/{id}', ['uses' => 'MemberController@delete']);
+});
