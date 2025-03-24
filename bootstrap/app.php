d194d3a37f63e3741246266f7b181370572acf44
B00177535 Nora Klarin <B00177535@myTUDublin.ie>
2025-03-24 14:19:23 +0000

    middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -84,13 +84,15 @@ $app->configure('app');
 |--------------------------------------------------------------------------
 | Register Service Providers
 |--------------------------------------------------------------------------
-|
+
 | Here we will register all of the application's service providers which
 | are used to bind services into the container. Service providers are
 | totally optional, so you are not required to uncomment this line.
 |
 */
-
+$app->middleware([
+    App\Http\Middleware\CorsMiddleware::class
+ ]);
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
@@ -113,3 +115,4 @@ $app->router->group([
 });
 
 return $app;
+
